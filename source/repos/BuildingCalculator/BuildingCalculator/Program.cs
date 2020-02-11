63526f8158f79a206624420e6536791fe13d4ab8
nod95 <deivissms@gmail.com>
2020-02-11 18:50:16 +0200

    building calculator

---
+++ source/repos/BuildingCalculator/BuildingCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BuildingCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32("45");
+            Console.WriteLine("|||Calculator|||");
+            Console.Write("Enter a number ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1);
+            Console.WriteLine("Enter a number or action");
+            int action =Convert.ToInt32(Console.ReadLine());
+            if (action == ) {
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
